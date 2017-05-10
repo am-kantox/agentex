959c399fc231f9efa5d409b96bba1839a2f08291
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-05-10 08:59:28 +0200

    Just to save an attempt.

--- lib/agentex/db.ex
+++ lib/agentex/db.ex
@@ -1,12 +1,12 @@
 defmodule Agentex.DB do
   @moduledoc false
   defmacro __using__(opts \\ []) do
-    quote bind_quoted: [opts: opts, module: __MODULE__] do
+    quote do
       Module.register_attribute __MODULE__, :database, accumulate: false
-      Module.put_attribute __MODULE__, :database, opts[:database] || AgentexDB
+      Module.put_attribute __MODULE__, :database, unquote(opts[:database]) || AgentexDB
 
       Module.register_attribute __MODULE__, :table, accumulate: true
-      Module.put_attribute __MODULE__, :table, opts[:table] || [Kv]
+      Module.put_attribute __MODULE__, :table, unquote(opts[:table]) || [Kv]
 
       database = Module.get_attribute(__MODULE__, :database)
       tables = Agentex.Namer.tables(Module.get_attribute(__MODULE__, :table))
@@ -17,10 +17,15 @@ defmodule Agentex.DB do
         end
       end)
 
-      quote do
-        defdatabase unquote(database) do
-          unquote(deftables)
-        end
+      defdatabase unquote(Module.get_attribute(__MODULE__, :database)) do
+        unquote(quote do
+          Enum.map(unquote(tables), fn table_name ->
+            quote do
+              deftable unquote(table_name), [:key, :value], type: :ordered_set, index: [:value] do
+              end
+            end
+          end)
+        end)
       end
     end
   end
